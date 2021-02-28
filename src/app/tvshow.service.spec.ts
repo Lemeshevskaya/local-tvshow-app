39ff787471bf740dc54f9e243fa5c23efb56a6e1
Lemeshevskaya <birtochka@gmail.com>
2021-02-28 10:24:47 -0800

    check test files

--- src/app/tvshow.service.spec.ts
+++ src/app/tvshow.service.spec.ts
@@ -1,12 +1,18 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { TvshowService } from './tvshow.service';
+
 
 describe('TvshowService', () => {
   let service: TvshowService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(TvshowService);
   });
 
